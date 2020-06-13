d9fc70d3c40ddae535e85467080f1038b4586127
Andyintae <runrunintae@gmail.com>
2020-06-13 10:25:03 +0900

    1st

---
+++ plot1.r
@@ -0,0 +1,14 @@
+library(tidyverse)
+
+
+# 1. Create wd, Download and unzip file ----------------------------------------------
+
+if(!file.exists("EDA")){dir.create("EDA")}
+setwd("./EDA")
+wd <- getwd()
+
+file <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(file, destfile = paste0(wd,"/household_power_consumption.zip"))
+unzip(zipfile = "household_power_consumption.zip")
+
+
